4df7288a074fcaa238f5471fa546431784f66d7f
Diego Santamaria <jd.santamariab1@uniandes.edu.co>
2025-04-25 05:31:38 -0500

    feat: HU01 - Add button to main activity that redirects to album catalog

--- app/src/main/java/com/example/vynilsapp/ListAlbumCatalogActivity.kt
+++ app/src/main/java/com/example/vynilsapp/ListAlbumCatalogActivity.kt
@@ -1,30 +1,20 @@
 package com.example.vynilsapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ListAlbumCatalogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list_album_catalog)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        // Set button click listeners
-        val buttonListAlbumCatalog = findViewById<AppCompatButton>(R.id.btn_see_album_catalog)
-        buttonListAlbumCatalog.setOnClickListener {
-            // Start ListAlbumCatalogActivity
-            Log.i("MainActivity", "Button clicked: List Album Catalog")
-            startActivity(Intent(this, ListAlbumCatalogActivity::class.java))
         }
     }
 }
