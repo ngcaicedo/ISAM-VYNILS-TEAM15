a798549db6f94465e37f787a7b5c3c3719143edc
Jose <jd.rodriguezg1234567@uniandes.edu.co>
2025-05-06 15:02:52 -0500

    Se desarrolla funcionalidad de listar artistas

---
+++ app/src/main/java/com/example/vynilsapp/models/Performer.kt
@@ -0,0 +1,16 @@
+package com.example.vynilsapp.models
+
+import com.google.gson.annotations.SerializedName
+
+data class Performer (
+    @SerializedName("id")
+    val performerId: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("image")
+    val image: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("type")
+    val type: String
+)
---
+++ app/src/main/java/com/example/vynilsapp/ui/PerformerFragment.kt
@@ -0,0 +1,92 @@
+package com.example.vynilsapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vynilsapp.R
+import com.example.vynilsapp.databinding.FragmentPerformerBinding
+import com.example.vynilsapp.ui.adapters.PerformersAdapter
+import com.example.vynilsapp.viewmodels.PerformerViewModel
+
+class PerformerFragment : Fragment() {
+    private var _binding: FragmentPerformerBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewModel: PerformerViewModel
+    private var viewModelAdapter: PerformersAdapter? = null
+
+    companion object {
+        // Bandera para indicar si es necesario refrescar los datos
+        var shouldRefreshData = false
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPerformerBinding.inflate(inflater, container, false)
+        val view = binding.root
+        viewModelAdapter = PerformersAdapter()
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Configurar RecyclerView
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = GridLayoutManager(context, 2) // 2 columnas
+        recyclerView.adapter = viewModelAdapter
+
+        // Inicializar ViewModel
+        val activity = requireActivity()
+        activity.actionBar?.title = getString(R.string.app_name)
+
+        viewModel = ViewModelProvider(this, PerformerViewModel.Factory(activity.application))
+            .get(PerformerViewModel::class.java)
+
+        // Observar cambios en el ViewModel
+        viewModel.performers.observe(viewLifecycleOwner) { performers ->
+            performers?.let {
+                viewModelAdapter!!.performers = it
+                binding.progressCircular.visibility = View.GONE
+            }
+        }
+
+        viewModel.eventNetworkError.observe(viewLifecycleOwner) { isNetworkError ->
+            if (isNetworkError) onNetworkError()
+        }
+    }
+
+    // Modificar método onResume para refrescar solo cuando sea necesario
+    override fun onResume() {
+        super.onResume()
+        // Solo refrescar si la bandera está activada
+        if (shouldRefreshData) {
+            // Mostrar el indicador de carga
+            binding.progressCircular.visibility = View.VISIBLE
+            // Refrescar datos desde la API
+            viewModel.refreshDataFromNetwork()
+            // Restablecer la bandera
+            shouldRefreshData = false
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun onNetworkError() {
+        if(!viewModel.isNetworkErrorShown.value!!) {
+            Toast.makeText(activity, "Error de red", Toast.LENGTH_LONG).show()
+            viewModel.onNetworkErrorShown()
+        }
+    }
+}
